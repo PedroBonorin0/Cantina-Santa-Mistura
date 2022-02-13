56d53b11d85c6c48a2de8beba1f1031c3319facb
Daniel Muller <danielmrezende@yahoo.com.br>
2022-02-13 16:01:39 -0300

    implementando tabelaClientes

--- frontend/src/views/Clientes.vue
+++ frontend/src/views/Clientes.vue
@@ -2,14 +2,21 @@
   <div class="geral">
     <h1>Gerenciamento de Clientes</h1>
     <BarraPesquisa :infoBuscada="'Cliente'"/>
+    <div id="tabelaClientes">
+      <TabelaClientes />
+    </div>
   </div>
 
 </template>
 
 <script>
-import BarraPesquisa from '../components/BarraPesquisa.vue'
+
+// import BarraPesquisa from '../components/BarraPesquisa.vue'
+import TabelaClientes from '../components/TabelaClientes.vue'
+
+// import BarraPesquisa from '../components/BarraPesquisa.vue'
 export default {
-	components: { BarraPesquisa },
+	components: { TabelaClientes  },
   
 }
 </script>
